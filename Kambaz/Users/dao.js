d5f115c9370cfec4b18f6da948dd109715d60d6a
fariasn516 <145229665+fariasn516@users.noreply.github.com>
2025-12-02 15:12:47 -0500

    Finish A6

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -9,7 +9,7 @@ export default function UsersDao() {
   const findAllUsers = () => model.find();
   const findUserById = (userId) => model.findById(userId);
   const findUserByUsername = (username) =>  model.findOne({ username: username });
-  const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+  const findUserByCredentials =  (username, password) => model.findOne({ username, password });
   const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
   const deleteUser = (userId) => model.findByIdAndDelete( userId );
   const findUsersByRole = (role) => model.find({ role: role });
--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(
  cors({
     origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true
+    credentials: false
   })
 );
 Lab5(app);
